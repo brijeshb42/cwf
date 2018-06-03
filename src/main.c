86beba63421663f2de311c34805aa596f0a6f987
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2018-06-03 10:41:13 +0530

    Some more parsing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<http_parser.h>
-#include<map.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <http_parser.h>
+#include <map.h>
 
 char *test = "GET /brijeshb42/e241b1dc89d1801270f2adb08b2109ea HTTP/1.1\n\
 Host: gist.github.com\n\
@@ -19,7 +19,10 @@ Cache-Control: max-age=0";
 
 typedef struct {
 	map_str_t headers;
-} request;
+	char *url;
+	int method;
+	char *method_str;
+} Request;
 
 char *previous_header = NULL;
 int prev_header_length = 0;
@@ -27,51 +30,55 @@ int prev_header_length = 0;
 int on_header_field(http_parser *parser, const char *at, size_t length) {
 	previous_header = (char*)at;
 	prev_header_length = length;
-	// char *test = malloc(sizeof(char)*(length + 1));
-	// strncat(test, at, length);
-	// printf("Header field received: %s\n", test);
-	// printf("Header field received: %.*s, %zu\n", length, (at-length-1), length);
 	return 0;
 }
 
 int on_header_value(http_parser *parser, const char *at, size_t length) {
 	if (previous_header != NULL && prev_header_length) {
-		char *key = malloc(sizeof(char) * (prev_header_length));
-		char *value = malloc(sizeof(char) * (length));
-		strncat(key, previous_header, prev_header_length);
-		strncat(value, at, length);
-		// printf("%s(%d): %s(%zu)\n", key, prev_header_length, value, length);
-		request *req = (request*)parser->data;
+		char *key = malloc(sizeof(char) * (prev_header_length + 1));
+		char *value = malloc(sizeof(char) * (length + 1));
+		strncpy(key, previous_header, prev_header_length);
+		key[prev_header_length] = '\0';
+		strncpy(value, at, length);
+		value[length] = '\0';
+		Request *req = (Request*)parser->data;
 		map_set(&req->headers, key, value);
 	}
-	// previous_header = at;
-	// prev_header_length = length;
-	// char *test = malloc(sizeof(char)*(length + 1));
-	// strncat(test, at, length);
-	// printf("Header value received: %s\n", test);
-	// printf("Header field received: %.*s, %zu\n", length, (at-length-1), length);
 	return 0;
 }
 
-request *init_request() {
-	request *req = malloc(sizeof(request));
+int on_url(http_parser *parser, const char *at, size_t length) {
+	Request *req = (Request*)parser->data;
+	req->url = malloc(sizeof(char) * (length + 1));
+	strncpy(req->url, at, length);
+	return 0;
+}
+
+Request *init_request() {
+	Request *req = malloc(sizeof(Request));
+	req->url = NULL;
 	map_init(&req->headers);
 	return req;
 }
 
-void free_request(request *r) {
-	map_deinit(&r->headers);
-	free(r);
+void free_request(Request *req) {
+	map_deinit(&req->headers);
+	free(req->url);
+	free(req);
 }
 
-int main() {
-	printf("http-parser version - %d.%d.%d\n", HTTP_PARSER_VERSION_MAJOR, HTTP_PARSER_VERSION_MINOR, HTTP_PARSER_VERSION_PATCH);
-	http_parser *parser = (http_parser*)malloc(sizeof(http_parser));;
+Request *parse_request(char *text, size_t length) {
+	http_parser *parser = (http_parser*)malloc(sizeof(http_parser));
+
+	if (!parser) {
+		return NULL;
+	}
+
 	http_parser_settings settings_null = {
 		.on_message_begin = 0,
 		.on_header_field = *on_header_field,
 		.on_header_value = *on_header_value,
-		.on_url = 0,
+		.on_url = *on_url,
 		.on_status = 0,
 		.on_body = 0,
 		.on_headers_complete = 0,
@@ -79,13 +86,27 @@ int main() {
 		.on_chunk_header = 0,
 		.on_chunk_complete = 0,
 	};
-	request *r = init_request();
+	Request *r = init_request();
 	parser->data = r;
 	http_parser_init(parser, HTTP_REQUEST);
-	int nparsed = http_parser_execute(parser, &settings_null, test, 971);
-	printf("%d\n", nparsed);
-	printf("Method - %d - %d\n", parser->method, HTTP_GET);
-	printf("http version: %d.%d\n", parser->http_major, parser->http_minor);
+	int nparsed = http_parser_execute(parser, &settings_null, text, length);
+	if (nparsed != length) {
+		return NULL;
+	}
+	r->method = parser->method;
+	free(parser);
+	return r;
+}
+
+int main() {
+	Request *r = parse_request(test, 516);
+
+	if (r == NULL) {
+		exit(1);
+	}
+
+	printf("URL - %s\n", r->url);
+	printf("Method - %d - %d\n", r->method, HTTP_GET);
 	const char *key;
 	map_iter_t iter = map_iter(&r->headers);
 	
@@ -93,7 +114,6 @@ int main() {
 		printf("%s -> %s\n", key, *map_get(&r->headers, key));
 	}
 
-	free(r);
-	free(parser);
+	free_request(r);
 	return 0;
 }
